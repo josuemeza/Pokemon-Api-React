15b3c875f71dee9fb5c11c2b3e4ef0b329b75eff
Josue Meza Peña <josuemezapena@Josues-MacBook-Pro.local>
2022-02-08 10:53:34 -0300

    chore: upgraded dependencies and removed unused component

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import { Backdrop } from "./Backdrop";
 
 export const Modal = ({ children, show }) => {
   return <Fragment>{show && <div>{children}</div>}</Fragment>;
