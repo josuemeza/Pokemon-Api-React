18ced8ba161d7f0342e21a7d631a644933517d07
Josue Meza <josue.meza.p@gmail.com>
2022-02-10 09:34:22 -0300

    chore: app component refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useOnScreen } from "./Components/useOnScreen";
-import PokemonList from "./Components/PokemonList";
-import "./Components/SearchBar";
-import SearchBar from "./Components/SearchBar";
-import "./Components/AutoScroll";
-import AutoScroll from "./Components/AutoScroll";
-import "./Components/Modal/ModalComponent";
-import ModalComponent from "./Components/Modal/ModalComponent";
-import { PokemonRestApi } from "./Services/Pokemon";
+import React, { useState } from "react"
+import { useOnScreen } from "./Components/useOnScreen"
+import PokemonList from "./Components/PokemonList"
+import SearchBar from "./Components/SearchBar"
+import AutoScroll from "./Components/AutoScroll"
+import ModalComponent from "./Components/Modal/ModalComponent"
+import { PokemonRestApi } from "./Services/Pokemon"
 
-function App() {
-  const [arrayCount, setRef] = useOnScreen({ rootMargin: "10px" });
-  const [searchName, setSearchName] = useState("");
-  const [modal, setModal] = useState({
-    show: false,
-    name: "",
-    pic: "",
-  });
+const defaultModal = {
+  show: false,
+  name: "",
+  pic: "",
+}
 
-  const [isEnd, setIsEnd] = useState(false);
+const App: React.FC = () => {
+  const [arrayCount, setRef] = useOnScreen({ rootMargin: "10px" })
+  const [searchName, setSearchName] = useState("")
+  const [modal, setModal] = useState(defaultModal)
+  const [isEnd, setIsEnd] = useState(false)
+
   return (
     <>
       <ModalComponent setModal={setModal} showModal={modal} />
@@ -32,7 +31,7 @@ function App() {
       </div>
       <AutoScroll isEnd={isEnd} setRef={setRef} />
     </>
-  );
+  )
 }
 
 export default App;
